f8a14b8e32a3b855c3833474a38c49fdc33c2ae7
Daniel Ricq <dricq@findusapp.de>
2022-08-30 21:15:28 +0200

    feat: Add remaining markup

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,25 @@
+import { TemperatureDisplay } from "./components/TemperatureDisplay";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <h1>Thermometer App</h1>
+      <TemperatureDisplay
+        heading="Bedroom"
+        temperature={26}
+      ></TemperatureDisplay>
+      <TemperatureDisplay
+        heading="Child's room"
+        temperature={25}
+      ></TemperatureDisplay>
+      <TemperatureDisplay
+        heading="Workspace"
+        temperature={24}
+      ></TemperatureDisplay>
+      <TemperatureDisplay
+        heading="Living Room"
+        temperature={27}
+      ></TemperatureDisplay>
+    </>
+  );
 }
