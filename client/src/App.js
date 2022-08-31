db0588e5708b06ec69e112e0bfb500f391788452
Daniel Ricq <dricq@findusapp.de>
2022-08-31 16:33:28 +0200

    feat[client]: Subscribe mqtt topics

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,72 @@
+import {
+  TemperatureSensorEnum,
+  evalEnum,
+  getTopic,
+} from "./types/TemperatureSensorEnum";
+import { useEffect, useState } from "react";
+
 import { TemperatureDisplay } from "./components/TemperatureDisplay";
+import mqtt from "precompiled-mqtt";
 
 export default function App() {
+  const [bedroomTemperature, setBedroomTemperature] = useState("-");
+  const [childsRoomTemperature, setChildsRoomTemperature] = useState("-");
+  const [workspaceTemperature, setWorkspaceTemperature] = useState("-");
+  const [livingRoomTemperature, setLivingRoomTemperature] = useState("-");
+
+  useEffect(() => {
+    const client = mqtt.connect("mqtt://test.mosquitto.org:8080");
+
+    client.on("connect", function () {
+      Object.values(TemperatureSensorEnum).forEach((temperatureSensorEnum) =>
+        client.subscribe(
+          getTopic({ temperatureSensorEnum: temperatureSensorEnum })
+        )
+      );
+
+      client.on("message", async (topic, message) => {
+        const temperature = await JSON.parse(message.toString()).temperature;
+        switch (evalEnum({ topic: topic })) {
+          case TemperatureSensorEnum.BEDROOM:
+            setBedroomTemperature(temperature);
+            break;
+          case TemperatureSensorEnum.CHILDS_ROOM:
+            setChildsRoomTemperature(temperature);
+            break;
+          case TemperatureSensorEnum.LIVING_ROOM:
+            setLivingRoomTemperature(temperature);
+            break;
+          case TemperatureSensorEnum.WORKSPACE:
+            setWorkspaceTemperature(temperature);
+            break;
+          default:
+            console.log("Unknown topic");
+        }
+      });
+    });
+  }, []);
+
   return (
-    <div class="w-screen h-screen bg-gradient-to-r from-cyan-500 to-blue-500 grid place-content-center">
-      <main class="grid grid-cols-2 gap-2 sm:gap-5 place-content-center bg-white shadow-xl rounded p-3 sm:p-5">
+    <div className="w-screen h-screen bg-gradient-to-r from-cyan-500 to-blue-500 grid place-content-center">
+      <main className="grid grid-cols-2 gap-2 sm:gap-5 place-content-center bg-white shadow-xl rounded p-3 sm:p-5">
         <h1 className="grid col-span-2 justify-self-center text-2xl mb-2 sm:mb-0">
           Thermometer App
         </h1>
         <TemperatureDisplay
           heading="Bedroom"
-          temperature={26}
+          temperature={bedroomTemperature}
         ></TemperatureDisplay>
         <TemperatureDisplay
           heading="Child's room"
-          temperature={25}
+          temperature={childsRoomTemperature}
         ></TemperatureDisplay>
         <TemperatureDisplay
           heading="Workspace"
-          temperature={24}
+          temperature={workspaceTemperature}
         ></TemperatureDisplay>
         <TemperatureDisplay
           heading="Living Room"
-          temperature={27}
+          temperature={livingRoomTemperature}
         ></TemperatureDisplay>
       </main>
     </div>
